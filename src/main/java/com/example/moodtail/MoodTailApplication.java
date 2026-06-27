0d9d256de93fb1264d6c953400a2170ba6bfd270
pumisj <pumisj@naver.com>
2026-06-27 22:04:54 +0900

    chore: 프로젝트 기본 세팅

---
+++ src/main/java/com/example/moodtail/MoodTailApplication.java
@@ -0,0 +1,22 @@
+package com.example.moodtail;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.TimeZone;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class MoodTailApplication {
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoodTailApplication.class, args);
+	}
+}
