c308abfd223681975af6162548f5c1fe2044f7a3
tjfdlvTkr <junha212@gmail.com>
2026-07-19 23:34:12 +0900

    chore: 인증 환경 설정을 필수 항목으로 정리

--- src/main/java/com/example/moodtail/MoodTailApplication.java
+++ src/main/java/com/example/moodtail/MoodTailApplication.java
@@ -5,14 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.TimeZone;
 
 @ConfigurationPropertiesScan
 @EnableJpaAuditing
 @SpringBootApplication
-@EnableScheduling
 public class MoodTailApplication {
 
 	@PostConstruct
