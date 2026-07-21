cce01402f6e01542b8d3d2a384fa64121bad5074
˙𐃷˙ <sodang0645@gmail.com>
2026-07-21 17:54:59 +0900

    chore : 서버 준비용 application.yaml 수정, SwaggerConfig.java 수정

--- src/main/java/com/example/moodtail/global/config/SwaggerConfig.java
+++ src/main/java/com/example/moodtail/global/config/SwaggerConfig.java
@@ -2,18 +2,33 @@ package com.example.moodtail.global.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${app.server-url}")
+    private String serverUrl;
+
     @Bean
     public OpenAPI openAPI() {
-        String jwtSchemeName = "bearerAuth";
+        Info info = new Info()
+                .version("v0")
+                .title("MoodTail API")
+                .description("MoodTail 백엔드 API 명세");
 
+        Server server = new Server();
+        server.setUrl(serverUrl);
+
+        String jwtSchemeName = "bearerAuth";
         SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
 
         Components components = new Components()
@@ -25,6 +40,8 @@ public class SwaggerConfig {
                                 .bearerFormat("JWT"));
 
         return new OpenAPI()
+                .info(info)
+                .servers(List.of(server))
                 .addSecurityItem(securityRequirement)
                 .components(components);
     }
