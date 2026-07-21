50d3de8b6c33f9d2d9afdd7dededcadb48e097f7
yongyong213 <yongyong213@naver.com>
2026-07-22 03:21:26 +0900

    feat: 도감 공유 api

---
+++ src/main/java/com/example/moodtail/domain/collection/dto/response/CollectionShareCreateResponse.java
@@ -0,0 +1,7 @@
+package com.example.moodtail.domain.collection.dto.response;
+
+public record CollectionShareCreateResponse(
+        String shareToken,
+        String shareUrl
+) {
+}
---
+++ src/main/java/com/example/moodtail/global/common/exception/code/status/CollectionErrorStatus.java
@@ -0,0 +1,29 @@
+package com.example.moodtail.global.common.exception.code.status;
+
+import com.example.moodtail.global.common.exception.code.BaseCodeDto;
+import com.example.moodtail.global.common.exception.code.BaseCodeInterface;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum CollectionErrorStatus implements BaseCodeInterface {
+
+    COLLECTION_SHARE_NOT_FOUND(HttpStatus.NOT_FOUND,"COLLECTION_SHARE404","공유된 도감을 찾을 수 없습니다.");
+
+    private final HttpStatus httpStatus;
+    private final boolean isSuccess = false;
+    private final String code;
+    private final String message;
+
+    @Override
+    public BaseCodeDto getCode() {
+        return BaseCodeDto.builder()
+                .httpStatus(httpStatus)
+                .isSuccess(isSuccess)
+                .code(code)
+                .message(message)
+                .build();
+    }
+}
