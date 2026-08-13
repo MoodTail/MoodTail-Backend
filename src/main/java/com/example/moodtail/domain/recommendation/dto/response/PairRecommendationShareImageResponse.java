d6554034d9066ef8284978cccceda5e31cd741c2
˙𐃷˙ <sodang0645@gmail.com>
2026-08-13 21:24:41 +0900

    fix : 같이 고르기 s3 경로 수정

--- src/main/java/com/example/moodtail/domain/recommendation/dto/response/PairRecommendationShareImageResponse.java
+++ src/main/java/com/example/moodtail/domain/recommendation/dto/response/PairRecommendationShareImageResponse.java
@@ -3,7 +3,7 @@ package com.example.moodtail.domain.recommendation.dto.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record PairRecommendationShareImageResponse(
-        @Schema(description = "S3에 저장된 페어 추천 공유 이미지 URL", example = "https://moodtail-bucket.s3.ap-northeast-2.amazonaws.com/recommendations/pair/9f3ab21c-....png")
+        @Schema(description = "S3에 저장된 페어 추천 공유 이미지 URL", example = "https://moodtail-bucket.s3.ap-northeast-2.amazonaws.com/public/pair/9f3ab21c-....png")
         String shareImageUrl
 ) {
 }
